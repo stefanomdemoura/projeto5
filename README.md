f3d581804d8662cd2eb5ede285ba568d6ed6b328
stefanomdemoura <309103395+stefanomdemoura@users.noreply.github.com>
2026-08-15 10:47:10 -0300

    adiciona cabecalho e botoes de histograma e dispersao no dashboard

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projeto5
 Projeto 5 (sprint 5)
+## app.py
+### Aplicativo que gera histogramas e gráficos de dispersão para interagir com preços de veículos
--- app.py
+++ app.py
@@ -5,4 +5,14 @@ import plotly_express as px
 vehicles = pd.read_csv('vehicles.csv')
 
 st.header('Análise de anúncios de veículos')
-hist_button = st.button('Criar gráfico')
+hist_button = st.button('Criar histograma de preços')
+disp_button = st.button('Criar gráfico de dispersão')
+
+if hist_button:
+    fig = px.histogram(vehicles, x='price')
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+    fig = px.scatter(vehicles, x='year', y='price',
+                     color='fuel', hover_data=['model'])
+    st.plotly_chart(fig, use_container_width=True)
