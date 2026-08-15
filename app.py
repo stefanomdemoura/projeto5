193bcb1427d7d259aecec6c01a7eae7864a41aac
stefanomdemoura <309103395+stefanomdemoura@users.noreply.github.com>
2026-08-15 11:08:52 -0300

    testando app 1

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import plotly_express as px
 
 vehicles = pd.read_csv('vehicles.csv')
 
+vehicles['model_year'] = vehicles['model_year'].astype(int)
+
 st.header('Análise de anúncios de veículos')
 hist_button = st.button('Criar histograma de preços')
 disp_button = st.button('Criar gráfico de dispersão')
@@ -13,6 +15,6 @@ if hist_button:
     st.plotly_chart(fig, use_container_width=True)
 
 if disp_button:
-    fig = px.scatter(vehicles, x='year', y='price',
+    fig = px.scatter(vehicles, x='model_year', y='price',
                      color='fuel', hover_data=['model'])
     st.plotly_chart(fig, use_container_width=True)
